af06d24d47cbfcfb2c855145e39e5b13af5a93bb
n-e-d-a <87589027+n-e-d-a@users.noreply.github.com>
2021-10-17 08:21:59 +0330

    hosted on Netlify added

--- src/App.js
+++ src/App.js
@@ -6,21 +6,29 @@ export default function App() {
     <div className="App">
       weather app
       <footer>
-        This project was coded by 
+        This project was coded by {""}
         <a
           href="https://xenodochial-poitras-5501a5.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-           Neda Zare 
+          Neda Zare
         </a>
-        and is
+        {""} and is {""}
         <a
           href="https://xenodochial-poitras-5501a5.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Open-Sourced on GitHub
+        </a>
+        {""} and {""} 
+        <a
+          href="https://inspiring-lamport-23bc03.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Hosted on  Netlify
         </a>
       </footer>
     </div>
