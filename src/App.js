bb435bc243b6af3c6961bcc993deb7172578f878
n-e-d-a <87589027+n-e-d-a@users.noreply.github.com>
2021-10-17 08:13:19 +0330

    project Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      weather app
+      <footer>
+        This project was coded by 
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://xenodochial-poitras-5501a5.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Neda Zare 
         </a>
-      </header>
+        and is
+        <a
+          href="https://xenodochial-poitras-5501a5.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-Sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
