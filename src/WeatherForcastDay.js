16281218055fefa6817b6947a3234570ff7773f0
n-e-d-a <87589027+n-e-d-a@users.noreply.github.com>
2021-10-31 08:05:39 +0330

    fix2

--- src/WeatherForcastDay.js
+++ src/WeatherForcastDay.js
@@ -4,5 +4,5 @@ export default function WeatherForcastDay(props) {
   const date = new Date(props.data.dt * 1000);
   const day = date.getDay();
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return <div>days[day]</div>;
+  return <div>{days[day]}</div>;
 }
