d877b7aa947ae02c462c0a82d5964d1beddfd8e1
n-e-d-a <87589027+n-e-d-a@users.noreply.github.com>
2021-11-02 07:00:21 +0330

    fixing

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -3,17 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./Circular.css";
 
 export default function WeatherTemperature(props) {
-  const [units, setUnits] = useState("celsius");
-  
-  
-    return (
-      <div>
-        {" "}
-        <span className="temperature">{props.celsius}</span>
-        <span className="units">
-          °C 
-         
-        </span>
-      </div>
-    );
-    }
+  return (
+    <div>
+      {" "}
+      <span className="temperature">{props.celsius}</span>
+      <span className="units">°C</span>
+    </div>
+  );
+}
